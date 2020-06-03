64970ea174342c154bf0137d08c0524923d2891d
Scott Key <scottdkey@gmail.com>
2020-06-03 17:53:21 -0600

    Capistrano installed

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,3 @@
-# frozen_string_literal: true
-
-server 'sample-web-server', user: 'deploy', roles: %w[app db web]
-
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -10,6 +6,8 @@ server 'sample-web-server', user: 'deploy', roles: %w[app db web]
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+
+
 
 # role-based syntax
 # ==================
@@ -23,6 +21,8 @@ server 'sample-web-server', user: 'deploy', roles: %w[app db web]
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
+
+
 # Configuration
 # =============
 # You can set any configuration variable like in config/deploy.rb
@@ -30,6 +30,8 @@ server 'sample-web-server', user: 'deploy', roles: %w[app db web]
 # For available Capistrano configuration variables see the documentation page.
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
+
+
 
 # Custom SSH Options
 # ==================
